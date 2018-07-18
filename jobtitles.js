6200e181df5d86f5d18cf5d9d2b83018e29bed9b
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-18 13:49:39 -0700

    logged jobtitles to console

--- jobtitles.js
+++ jobtitles.js
@@ -1,18 +1,25 @@
+'use strict';
 const myArr = [
-  knownPeople1 = {
+  {
     name: 'Jem',
     jobTitle: 'Pro programmer'
   },
-  knownPeople2 = {
+  {
     name: 'Frodo',
     jobTitle: 'Ring Destroyer'
   },
-  knownPeople3 = {
+  {
     name: 'DarthVader',
     jobTitle: 'Rebel defeater'
   },
-  knownPeople4 = {
+  {
     name: 'Elon Musk',
     jobTitle: 'CEO'
   }
 ];
+
+myArr.forEach(function(item) {
+  console.log(`${item.name}: ${item.jobTitle}`);
+});
+
+

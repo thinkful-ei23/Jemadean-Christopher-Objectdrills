a47658583fe86a5e74299105627a93902059070a
Chris Gantz <chrisgantz836@gmail.com>
2018-07-18 13:30:18 -0700

    unfinshed js file

---
+++ jobtitles.js
@@ -0,0 +1,18 @@
+const myArr = [
+  knownPeople1 = {
+    name: 'Jem',
+    jobTitle: 'Pro programmer'
+  },
+  knownPeople2 = {
+    name: 'Frodo',
+    jobTitle: 'Ring Destroyer'
+  },
+  knownPeople3 = {
+    name: 'DarthVader',
+    jobTitle: 'Rebel defeater'
+  },
+  knownPeople4 = {
+    name: 'Elon Musk',
+    jobTitle: 'CEO'
+  }
+];

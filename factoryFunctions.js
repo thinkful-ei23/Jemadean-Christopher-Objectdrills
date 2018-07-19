b16c336269e3471e0a100f5881639e182aebec60
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-19 00:10:32 -0700

    stopped at creating array

--- factoryFunctions.js
+++ factoryFunctions.js
@@ -17,75 +17,83 @@ function createCharacter(name) {
   };
 }
 
-const characters = [
-    {
-        name: 'Gandalf the White',
-        nickname: 'gandalf',
-        race: 'Wizard',
-        origin: 'Middle Earth',
-        attack: 10
-        defense: 6
-        x: function() {
-            return (this.attack > this.defense ? 0 : this.defense - this.attack);
-        },
-        y: function() {
-            return (this.defense > this.attack ? 0 : this.attack - this.defense);
-        }    
-    },
-    {
-        name: 
-        nickname:
-        race: 
-        origin: 
-        attack:
-        defense: 
-        x: function() {
-            return (this.attack > this.defense ? 0 : this.defense - this.attack);
-        },
-        y: function() {
-            return (this.defense > this.attack ? 0 : this.attack - this.defense);
-        }    
-    },
-    {
-        name: 
-        nickname:
-        race: 
-        origin: 
-        attack:
-        defense: 
-        x: function() {
-            return (this.attack > this.defense ? 0 : this.defense - this.attack);
-        },
-        y: function() {
-            return (this.defense > this.attack ? 0 : this.attack - this.defense);
-        }    
-    },
-    {
-        name: 
-        nickname:
-        race: 
-        origin: 
-        attack:
-        defense: 
-        x: function() {
-            return (this.attack > this.defense ? 0 : this.defense - this.attack);
-        },
-        y: function() {
-            return (this.defense > this.attack ? 0 : this.attack - this.defense);
-        }    
-    },
-    {
-        name: 
-        nickname:
-        race: 
-        origin: 
-        attack:
-        defense: 
-        x: function() {
-            return (this.attack > this.defense ? 0 : this.defense - this.attack);
-        },
-        y: function() {
-            return (this.defense > this.attack ? 0 : this.attack - this.defense);
-        }    
-    }
-]
+// creating and array characters that calls the factory function is throwing me off...all below here is ideas
+// for that but not fully formed...stopping for now...
+
+// const characters = createCharacter(name, nickname, race, origin, attack, defense) {
+    
+// }
+
+
+// const characters = [
+//     {
+//         name: 'Gandalf the White',
+//         nickname: 'gandalf',
+//         race: 'Wizard',
+//         origin: 'Middle Earth',
+//         attack: 10
+//         defense: 6
+//         x: function() {
+//             return (this.attack > this.defense ? 0 : this.defense - this.attack);
+//         },
+//         y: function() {
+//             return (this.defense > this.attack ? 0 : this.attack - this.defense);
+//         }    
+//     },
+//     {
+//         name: 
+//         nickname:
+//         race: 
+//         origin: 
+//         attack:
+//         defense: 
+//         x: function() {
+//             return (this.attack > this.defense ? 0 : this.defense - this.attack);
+//         },
+//         y: function() {
+//             return (this.defense > this.attack ? 0 : this.attack - this.defense);
+//         }    
+//     },
+//     {
+//         name: 
+//         nickname:
+//         race: 
+//         origin: 
+//         attack:
+//         defense: 
+//         x: function() {
+//             return (this.attack > this.defense ? 0 : this.defense - this.attack);
+//         },
+//         y: function() {
+//             return (this.defense > this.attack ? 0 : this.attack - this.defense);
+//         }    
+//     },
+//     {
+//         name: 
+//         nickname:
+//         race: 
+//         origin: 
+//         attack:
+//         defense: 
+//         x: function() {
+//             return (this.attack > this.defense ? 0 : this.defense - this.attack);
+//         },
+//         y: function() {
+//             return (this.defense > this.attack ? 0 : this.attack - this.defense);
+//         }    
+//     },
+//     {
+//         name: 
+//         nickname:
+//         race: 
+//         origin: 
+//         attack:
+//         defense: 
+//         x: function() {
+//             return (this.attack > this.defense ? 0 : this.defense - this.attack);
+//         },
+//         y: function() {
+//             return (this.defense > this.attack ? 0 : this.attack - this.defense);
+//         }    
+//     }
+// ]

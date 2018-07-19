4d0aa147ece3cc882cb4cfee4391a0f1bdf2d497
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-18 21:33:46 -0700

    fixed minor syntax errors

--- crackTheObject.js
+++ crackTheObject.js
@@ -1,22 +1,26 @@
+'use strict';
+
 const cypher = {
   a: 1,
   b: 2,
   c: 3,
   d: 4
-}
+};
 
 function decodeWords(words) {
   let firstChar = words.charAt(0);
   let askCypherForNum = cypher[firstChar];
-  return askCypherForNum !== undefined ? words[askCypherForNum] :  " "
+  return askCypherForNum !== undefined ? words[askCypherForNum] :  " ";
 }
 
 function decode(sentence) {
   let words = "";
-  let wordsArr = sentence.split(" ")
-  for (i = 0; i < wordsArr.length; i++) {
+  let wordsArr = sentence.split(" ");
+  for (let i = 0; i < wordsArr.length; i++) {
     words += decodeWords(wordsArr[i]);
   }
   return words;
 }
 console.log(decode("craft block argon meter bells brown croon droop"));
+
+
